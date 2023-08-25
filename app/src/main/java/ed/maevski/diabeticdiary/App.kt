8806954cc0a0dbcfd588f9f8244344f515f8c2f9
maevskiEd <maevski.ed@gmail.com>
2023-08-25 15:35:46 +0300

    sprint 2

--- app/src/main/java/ed/maevski/diabeticdiary/App.kt
+++ app/src/main/java/ed/maevski/diabeticdiary/App.kt
@@ -1,6 +1,23 @@
 package ed.maevski.diabeticdiary
 
 import android.app.Application
+import ed.maevski.diabeticdiary.di.AppComponent
+import ed.maevski.diabeticdiary.di.DaggerAppComponent
 
 class App : Application() {
+    lateinit var dagger: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+
+        instance = this
+
+    }
+
+    companion object {
+        //Здесь статически хранится ссылка на экземпляр App
+        lateinit var instance: App
+            //Приватный сеттер, чтобы нельзя было в эту переменную присвоить что-либо другое
+            private set
+    }
 }
---
+++ app/src/main/java/ed/maevski/diabeticdiary/data/YandexApi.kt
@@ -0,0 +1,8 @@
+package ed.maevski.diabeticdiary.data
+
+interface YandexApi {
+
+    object ApiConst {
+        const val BASE_URL = "tts.api.cloud.yandex.net"
+    }
+}
